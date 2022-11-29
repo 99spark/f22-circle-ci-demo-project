1902d818bc18c0dd6618cb26542106cda5b4804e
alexf <alexfischer57@yahoo.com>
2022-11-29 14:13:36 -0800

    added an endpoint and a test to verify I can access it

--- src/test/java/com/example/f22circlecidemoproject/F22CircleCiDemoProjectApplicationTests.java
+++ src/test/java/com/example/f22circlecidemoproject/F22CircleCiDemoProjectApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.f22circlecidemoproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
-class F22CircleCiDemoProjectApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class F22CircleCiDemoProjectApplicationTests
+{
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
-    void contextLoads() {
+    public void contextLoads()
+    {
+
     }
 
+    @Test
+    public void homeTest()
+    {
+        //create a request
+        String url = "http://localhost:" + port + "/api/v1/home";
+        HttpEntity request = new HttpEntity(new HttpHeaders());
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET,
+                request,String.class);
+
+        //test whether it worked or not?
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(),"Hello world");
+    }
 }
